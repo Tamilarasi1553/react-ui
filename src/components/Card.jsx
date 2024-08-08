1a364c24b68640c076d421d5856059b8e28a000d
praveen <yonas@PraveenTamil.com>
2024-08-08 13:53:09 +0530

    initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BiLogoHtml5 } from "react-icons/bi";
+import { BiBuilding } from "react-icons/bi";
+import { BiLogoAndroid } from "react-icons/bi";
+const course = [
+    {
+        title:"Web Development",
+        icon:<BiLogoHtml5 />,
+
+      
+    },
+    {
+        title:"App Development",
+        duration:"2 hours",
+        icon:<BiLogoAndroid />,
+    },
+    {
+        title:'UX & UI',
+        duration:'2 hours',
+        icon:<BiBuilding />,
+    },
+
+];
+
+const Card = () => {
+    return (
+        <div className="card-container">
+        {course.map((item)=> (
+            <div className="card">
+                <div className="card-cover">
+                    {item.icon}
+                </div>
+                <div className="card-title">
+                    <h2>{item.title}</h2>
+                </div>
+            </div>
+        ))}
+        </div>
+    );
+};
+
+export default Card;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { BiBookAlt } from "react-icons/bi";
+import { BiHome } from "react-icons/bi";
+import { MdAssignment } from "react-icons/md";
+import { BiSolidReport } from "react-icons/bi";
+import { BiStats } from "react-icons/bi";
+import { BiMessage } from "react-icons/bi";
+import { BiHelpCircle } from "react-icons/bi";
+
+import '../styles/sidebar.css';
+const Sidebar = () => {
+    return <div className='menu'>
+        <div className='logo'>
+        <BiBookAlt className="logo-icon"/>
+            <h2>EduFlex</h2>
+        </div>
+
+        <div className="menu-list ">
+            <a href="#" className="item active">
+            <BiHome className="icon"/>
+                Dashboard
+            </a>
+            <a href="#" className="item">
+            <MdAssignment className="icon" />
+                Assignment
+            </a>
+            <a href="#" className="item">
+            <BiSolidReport className="icon"/>
+                Report
+            </a>
+            <a href="#" className="item">
+            <BiStats className="icon"/>
+                Stats
+            </a>
+            <a href="#" className="item">
+            <BiMessage className="icon"/>
+                Message
+            </a>
+            <a href="#" className="item">
+            <BiHelpCircle className="icon"/>
+               Help
+            </a>
+        </div>
+    </div>
+}
+
+export default Sidebar
